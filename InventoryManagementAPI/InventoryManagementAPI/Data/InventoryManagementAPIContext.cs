b803bdb9a62b0a41ab707ca2bea5982a3cc9a02a
Tintin Falk <tintinfalk01@gmail.com>
2024-09-27 12:02:37 +0200

    small tweak with testdata

--- InventoryManagementAPI/InventoryManagementAPI/Data/InventoryManagementAPIContext.cs
+++ InventoryManagementAPI/InventoryManagementAPI/Data/InventoryManagementAPIContext.cs
@@ -56,7 +56,7 @@ namespace InventoryManagementAPI.Data
             {
                 Name = "Testprodukt",
                 ArticleNumber = "TP001",
-                CurrentStock = 0,
+                CurrentStock = 50,
                 TotalStock = 50,
                 Description = "Detta är en testprodukt.",
                 Price = 99.99m,
@@ -99,8 +99,6 @@ namespace InventoryManagementAPI.Data
                 ProductId = product.Id,
                 Quantity = 50
             };
-
-           
 
             InventoryTracker.AddRange(inventoryTracker1);
             await SaveChangesAsync(); 
